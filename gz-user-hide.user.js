96e16460cb5c78c40280639c58f8d6e255740f3b
Dan Faulknor <danielfaulknor@users.noreply.github.com>
2014-06-03 22:15:37 +1200

    Updating to hide quotes

--- gz-user-hide.user.js
+++ gz-user-hide.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name       Geekzone User Hide
-// @namespace  http://github.com/danielfaulknor/GZUserHide
+// @namespace  http://running-config.co.nz/GZUserHide
 // @version    0.2
 // @description  Alpha version, enter users into the array at the top to hide posts from them
 // @match      http://www.geekzone.co.nz/*
@@ -10,15 +10,24 @@
 // @downloadURL    http://running-config.co.nz/GZUserHide/release/gz-user-hide.user.js
 // ==/UserScript==
 
+//Must all be in lower case
 var arr = [ "danielfaulknor" ];
 
 $(document).ready(function() {
     $('a').each(function(i, obj) {
-        if ($(this).context.className == "subHeading _f_username")
-        	{
-                	if ($.inArray(obj.text, arr) > -1) {
-                		$(this).closest('tr').replaceWith( '<b>Hidden user removed</b><br />' );
-                	}
-            	}	
+       if ($(this).context.className == "subHeading _f_username")
+            {
+                if ($.inArray(obj.text.toLowerCase(), arr) > -1) {
+                    $(this).closest('tr').replaceWith( '<b>Hidden user removed</b><br />' );
+                }
+            }
 	});
+    
+    $('blockquote').each(function(i, obj) {
+        $.each(arr, function(index, item) {
+        	if (obj.innerText.toLowerCase().indexOf(item) >= 0) {
+            		obj.innerHTML = "<b>Quote containg post from hidden user removed</b>";
+        	}
+  	});
+    });
 });
